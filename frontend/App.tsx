087d8af00e72485723129d397ade1f08ba5cf779
Surdu Lucian <luciansurdu66@gmail.com>
2024-01-19 00:39:01 +0200

    Created the login screen, created the link from the home screen to login screen

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './src/screens/HomeScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
